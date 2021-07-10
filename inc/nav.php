c2b59e5e59589ef01e9a7abb6047624ea26fdd2a
Miranda Price <miranda27price@gmail.com>
2021-07-09 22:50:42 -0500

    init

---
+++ inc/nav.php
@@ -0,0 +1,20 @@
+<nav class="menu">
+	<ul>
+		<li>
+			<a href="index.php">Home</a>
+		</li>
+		<li>
+			<a href="food.php">Food</a>
+		</li>
+		<li>
+			<a href="museums.php">Museums</a>
+		</li>
+
+		<li>
+			<a href="attractions.php">Attractions</a>
+		</li>
+		<li>
+			<a href="gallery.php">Gallery</a>
+		</li>
+	</ul>
+</nav>
